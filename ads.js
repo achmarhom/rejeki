ba95b6c632de4a29d173f88c55281e46efd1ee5c
Ach Marhom <128275930+achmarhom@users.noreply.github.com>
2023-04-10 05:52:41 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,11 @@
+<div style="position: fixed; bottom: 0; left: 50%; transform: translateX(-50%);">
+   <script type="text/javascript">
+	atOptions = {
+		'key' : '5c5c6ef5a97b0b7e4cb5be2a1545aeb3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/5c5c6ef5a97b0b7e4cb5be2a1545aeb3/invoke.js"></scr' + 'ipt>');
+</script></div>
